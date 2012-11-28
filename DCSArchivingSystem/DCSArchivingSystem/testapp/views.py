e4ad8b67da85104373643a36bd36ad3402d25bab
Carl <carl.earvin.yu@gmail.com>
2012-11-29 03:27:03 +0800

    Changed Files

--- DCSArchivingSystem/DCSArchivingSystem/testapp/views.py
+++ DCSArchivingSystem/DCSArchivingSystem/testapp/views.py
@@ -4,14 +4,16 @@ from django.shortcuts import render_to_response
 
 # This is the function that is called by a url(...), in urls.py
 def main_page(request):
-    nameList = ['Noel', 'Cha']
-    lastEditedBy = 'Cha'
+    nameList = ['Noel', 'Cha', 'Carl']
+    lastEditedBy = 'Carl'
     message1 = 'blablabla'
     message2 = 'Sup, Carl'
+    message3= 'Hi, me!'
     # Display page without passing any variable
     # return render_to_response('index.html')
     
     # Display page while passing variables
     # by adding a "dictionary" argument like:
     # { 'nameInHtmlPage': variableName , 'nameInHtmlAgain': anotherVariableHere, ... }
-    return render_to_response('index.html', { 'nameList': nameList , 'lastEditedBy': lastEditedBy, 'fromNoel': message1, 'fromCha': message2 })
+    return render_to_response('index.html', { 'nameList': nameList , 'lastEditedBy': lastEditedBy, 'fromNoel': message1, 'fromCha': message2 , 'fromCarl'
+	:message3})
